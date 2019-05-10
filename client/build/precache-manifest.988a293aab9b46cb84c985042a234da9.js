7688d82e131e6b68bf9b288cfa91221a785ebf8d
Matt Andreko <mandreko@gmail.com>
2019-05-09 21:10:31 -0400

    Commited initial static build of client

---
+++ client/build/precache-manifest.988a293aab9b46cb84c985042a234da9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d43ec85c3ff29f6f03bf426e08544cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ad29b7d08a2cd2d1ae3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "db7847661ff5d65fe734",
+    "url": "/static/js/2.cdd55ca9.chunk.js"
+  },
+  {
+    "revision": "0ad29b7d08a2cd2d1ae3",
+    "url": "/static/js/main.2ea51ff2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
